9fe7dc248460b49da07e1a2de545d107b662e6f6
Davina Leong <davina.leong.sy@gmail.com>
2022-08-22 12:13:44 +0800

    Added data script

---
+++ scripts/data.js
@@ -0,0 +1,25 @@
+const data = {
+  pledges: [
+    {
+      title: "Bamboo Stand",
+      amount: 25,
+      description:
+        "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+      count: 101,
+    },
+    {
+      title: "Black Edition Stand",
+      amount: 75,
+      description:
+        "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+      count: 64,
+    },
+    {
+      title: "Mahogany Special Edition",
+      amount: 200,
+      description:
+        "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added  to our Backer member list. Shipping is included.",
+      count: 0,
+    },
+  ],
+}
