5396d1ad9205a7f765024daa65923a71d70a77e2
Davina Leong <davina.leong.sy@gmail.com>
2022-08-22 16:02:18 +0800

    Added toggle menu functionality

--- scripts/data.js
+++ scripts/data.js
@@ -23,3 +23,5 @@ const data = {
     },
   ],
 }
+
+console.log(`Data: `, data)
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-console.log(`Script loaded.`)
+const primaryToggle = document.querySelector(`[data-primary-toggle]`)
+const primaryHeader = document.querySelector(`[data-primary-header]`)
+
+primaryToggle.addEventListener(`click`, (e) => {
+  primaryHeader.toggleAttribute(`data-open`)
+})
