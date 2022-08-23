c5e918e5617bc0c1adc7cd2b4d9345852d774bce
Davina Leong <davina.leong.sy@gmail.com>
2022-08-23 14:46:27 +0800

    Load pledges data

--- scripts/data.js
+++ scripts/data.js
@@ -23,5 +23,3 @@ const data = {
     },
   ],
 }
-
-console.log(`Data: `, data)
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,45 @@
-const primaryToggle = document.querySelector(`[data-primary-toggle]`)
-const primaryHeader = document.querySelector(`[data-primary-header]`)
+const primaryHeaderEl = document.querySelector(`[data-primary-header]`)
+const primaryToggleEl = document.querySelector(`[data-primary-toggle]`)
 
-primaryToggle.addEventListener(`click`, (e) => {
-  primaryHeader.toggleAttribute(`data-open`)
+const pledgesEl = document.querySelector(`[data-pledges]`)
+
+primaryToggleEl.addEventListener(`click`, (e) => {
+  primaryHeaderEl.toggleAttribute(`data-open`)
 })
+
+const { pledges } = data
+let pledgesHtml = ``
+pledges.forEach((pledge) => {
+  const { title, amount, description, count } = pledge
+  let notAvailable = ``
+  let btnLabel = `Select Reward`
+  if (count <= 0) {
+    notAvailable = `data-not-available`
+    btnLabel = `Out of Stock`
+  }
+
+  pledgesHtml += `
+    <div class="pledge" ${notAvailable}>
+        <div class="pledge-header">
+            <h3 class="fw-bold">${title}</h3>
+            <p class="text-primary-400">Pledge $${amount} or more</p>
+        </div>
+
+        <div class="pledge-content">
+            <p class="text-neutral-600">${description}</p>
+        </div>
+
+        <div class="pledge-amount text-neutral-600">
+            <div class="text-neutral-900 fw-bold fs-700">${count}</div>
+            <div>left</div>
+        </div>
+
+        <div class="pledge-controls">
+            <button class="btn btn-primary" type="button">
+                ${btnLabel}
+            </button>
+        </div>
+    </div>
+    `
+})
+pledgesEl.innerHTML = pledgesHtml
