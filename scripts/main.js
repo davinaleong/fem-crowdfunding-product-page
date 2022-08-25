b2c2c3882c11ea0d6103af076c4e678aa1726b15
Davina Leong <davina.leong.sy@gmail.com>
2022-08-25 10:29:22 +0800

    Done with button toggles

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 const primaryHeaderEl = document.querySelector(`[data-primary-header]`)
 const btnPrimaryToggleEl = document.querySelector(`[data-btn-primary-toggle]`)
+const btnPledgeEls = document.querySelectorAll(`[data-btn-pledge]`)
 
 const btnBackProjectEl = document.querySelector(`[data-btn-back-project]`)
 const btnClosePledgeModalEl = document.querySelector(
@@ -8,19 +9,35 @@ const btnClosePledgeModalEl = document.querySelector(
 const modalPledgesEl = document.querySelector(`[data-modal-pledges]`)
 
 const pledgesEl = document.querySelector(`[data-pledges]`)
-const pledgesFormEl = document.querySelector(`[data-pledges-form]`)
+const pledgesFormEl = document.querySelector(`[data-form-pledges]`)
 const pledgeRadioEl = document.querySelectorAll(`input[name="pledge"]`)
 
+const btnContinueEls = document.querySelectorAll(`[data-btn-continue]`)
+
+const modalThankYouEl = document.querySelector(`[data-modal-thank-you]`)
+const btnCloseThankYouModalEl = document.querySelector(
+  `[data-btn-close-thank-you-modal]`
+)
+
+const openAttribute = `data-open`
+const selectedAttribute = `data-selected`
+
 btnPrimaryToggleEl.addEventListener(`click`, (e) => {
-  primaryHeaderEl.toggleAttribute(`data-open`)
+  primaryHeaderEl.toggleAttribute(openAttribute)
+})
+
+btnPledgeEls.forEach((el) => {
+  el.addEventListener(`click`, (e) => {
+    modalPledgesEl.setAttribute(openAttribute, true)
+  })
 })
 
 btnBackProjectEl.addEventListener(`click`, (e) => {
-  modalPledgesEl.setAttribute(`data-open`, true)
+  modalPledgesEl.setAttribute(openAttribute, true)
 })
 
 btnClosePledgeModalEl.addEventListener(`click`, (e) => {
-  modalPledgesEl.removeAttribute(`data-open`)
+  modalPledgesEl.removeAttribute(openAttribute)
 })
 
 pledgeRadioEl.forEach((el) => {
@@ -28,11 +45,22 @@ pledgeRadioEl.forEach((el) => {
     const selectedPledge = e.target.value
 
     document.querySelectorAll(`[data-selected]`).forEach((el) => {
-      el.removeAttribute(`data-selected`)
+      el.removeAttribute(selectedAttribute)
     })
 
     document
       .querySelector(`[data-key="${selectedPledge}"]`)
-      .setAttribute(`data-selected`, true)
+      .setAttribute(selectedAttribute, true)
   })
+})
+
+btnContinueEls.forEach((el) => {
+  el.addEventListener(`click`, (e) => {
+    modalPledgesEl.removeAttribute(openAttribute)
+    modalThankYouEl.setAttribute(openAttribute, true)
+  })
+})
+
+btnCloseThankYouModalEl.addEventListener(`click`, (e) => {
+  modalThankYouEl.removeAttribute(openAttribute)
 })
